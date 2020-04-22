acb6601bd26a19e540b7c37db43d0ea96823aa82
dbstjrwnekd <sh4869@ajou.ac.kr>
2020-04-23 03:59:42 +0900

    change

---
+++ boiler-plate2/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+const {User} = require('./models/User');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+//aplication/json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI,{
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true,
+    useFindAndModify:false
+}).then(()=>console.log("mongoDB connected..."))
+.catch(err=>console.log(err));
+
+app.get('/',(req,res) => res.send("hello world haha"));
+
+app.post('/register',(req,res) => {
+
+    //get informations from client & save data in database
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success:false,err});
+        return res.status(200).json({
+            success:true
+        })
+    });
+});
+
+app.listen(port, () => console.log(`port on ${port}`));
+
+
